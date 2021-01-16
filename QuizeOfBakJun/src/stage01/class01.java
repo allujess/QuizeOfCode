2c50b74a4ea76a794df7d65f26e8a030a9355e65
allujess <allujess95@gmail.com>
2021-01-16 23:11:11 +0900

    d연

---
+++ QuizeOfBakJun/src/stage01/class01.java
@@ -0,0 +1,132 @@
+package stage01;
+
+import java.util.Scanner;
+
+public class class01 {
+
+	public static void main(String[] args) {
+
+		//문제1
+	//	System.out.println("Hello World");
+		
+		//2
+	/*	System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군"); 	*/
+		
+		//3,4
+	/*	System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		System.out.println("===========");
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		System.out.println("");	*/
+		
+		//5
+	/*	Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		if(a > 0 && b < 10) {
+			int sum = a + b;
+			System.out.println(sum);
+			
+		}else {
+			System.out.println("조건을 맞춰주세요.");
+		}	*/
+		
+		//6
+	/*	Scanner scanner = new Scanner(System.in);
+				
+				int a = scanner.nextInt();
+				int b = scanner.nextInt();
+				
+				if(a > 0 && b < 10) {
+					int sum = a - b;
+					System.out.println(sum);
+					
+				}else {
+					System.out.println("조건을 맞춰주세요.");
+				} */
+		
+		//7		
+	/*	Scanner scanner = new Scanner(System.in);
+				
+				int a = scanner.nextInt();
+				int b = scanner.nextInt();
+				
+				if(a > 0 && b < 10) {
+					int sum = a * b;
+					System.out.println(sum);
+					
+				}else {
+					System.out.println("조건을 맞춰주세요.");
+				}*/
+		
+		//8
+	/*	Scanner scanner = new Scanner(System.in);
+				
+				double a = scanner.nextInt();
+				double b = scanner.nextInt();
+				
+				if(a > 0 && b < 10) {
+					double sum = a / b;
+					System.out.println(sum);
+					
+				}else {
+					System.out.println("조건을 맞춰주세요.");
+				} */
+		//9
+			/*	Scanner scanner = new Scanner(System.in);
+				
+						int a = scanner.nextInt();
+						int b = scanner.nextInt();
+				
+						if(a >= 1 && b <= 10000) {
+							int plus = a + b;
+							int minus = a - b;
+							int multifly = a * b;
+							int share = a / b;
+							int remainder = a % b ;
+							System.out.println(plus);
+							System.out.println(minus);
+							System.out.println(multifly);
+							System.out.println(share);
+							System.out.println(remainder);
+							
+							
+						}else {
+							System.out.println("조건을 맞춰주세요.");
+						}*/
+		
+			//10
+		/*	Scanner scanner = new Scanner(System.in);
+			
+						int a = scanner.nextInt();
+						int b = scanner.nextInt();
+						int c = scanner.nextInt();
+						
+						if(a >= 2 && c <= 10000) {
+							int first = (a+b)%c;
+							int second = ((a%c)+(b%c))%c;
+							int third = (a*b)%c;
+							int fourth = ((a%c)*(b%c))%c;
+							System.out.println(first);
+							System.out.println(second);
+							System.out.println(third);
+							System.out.println(fourth);
+						}else {
+							System.out.println("조건을 맞춰주세요.");
+						} */
+		
+		
+								
+				
+	}
+
+}
